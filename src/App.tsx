7791a0623956e5b39fff68f7f4e1bb88f43efc5a
christian1202 <basinillocj5@gmail.com>
2025-12-31 12:28:24 +0800

    fix: fixing the bugs

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import CreateEvent from "./pages/admin/CreateEvent";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import AddMember from "./pages/admin/AddMember";
 import EditMember from "./pages/admin/EditMember";
+import MyHistory from "./pages/student/MyHistory";
+import AttendanceReport from "./pages/admin/AttendanceReport";
 
 
 function App() {
@@ -21,13 +23,15 @@ function App() {
         <Route element={<DashboardLayout />}>
            
            {/* Student Pages */}
-           <Route path="/" element={<Navigate to="/dashboard" replace />} />
-           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/history" element={<MyHistory />} />
 
            {/* Admin Pages */}
-           <Route path="/admin/dashboard" element={<AdminDashboard />} />
-           <Route path="/admin/create-event" element={<CreateEvent />} />
-           <Route path="/admin/add-member" element={<AddMember />} />
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          <Route path="/admin/create-event" element={<CreateEvent />} />
+          <Route path="/admin/add-member" element={<AddMember />} />
+          <Route path="/admin/attendance-report" element={<AttendanceReport />} />
            
            {/* 👇 HERE IT IS! The new dynamic route */}
            <Route path="/admin/edit-member/:id" element={<EditMember />} />
